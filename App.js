ffc2ea3848eb2a9bbe3c64610f5d1a7670053d5c
Palmito254 <bgr921@myy.haaga-helia.fi>
2022-10-12 00:18:54 +0300

    f

--- App.js
+++ App.js
@@ -1,11 +1,24 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button, TextInput } from 'react-native';
+import { useState } from 'react';
+import * as Speech from 'expo-speech';
 
 export default function App() {
+
+  const [text, setText] = useState("");
+
+  const speak = () => {
+    Speech.speak(text);
+  };
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <TextInput
+        style={{ borderColor: "gray", borderWidth: 1, width: 300, height: 70}}
+        onChangeText={text => setText(text)}
+        value={text}
+      />
+      <Text></Text>
+      <Button title='Press me!' onPress={speak} />
     </View>
   );
 }
